a9c3d52b5a444745a66c78bc750fd8e9db584c6c
Jackson Alves <jacksonalves541@gmail.com>
2022-01-15 10:44:17 -0300

    change structure of project

--- __init__.py
+++ __init__.py
@@ -1,21 +1,19 @@
-from flask import Flask, request, Response
+import os
+from flask import Flask
+from . import main
 
-secret_access = '7d847723-f866-4ead-9f3a-ffdd26c59e2c'
-app = Flask(__name__, instance_relative_config=True)
+def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
 
-@app.route('/', methods=['POST',])
-def hello():
-    signature_256 = request.headers['X-Hub-Signature-256']
-    if not verify_signature(secret_access, signature_256, request.data):
-        return Response(status=403)
-    return Response(status=200)
+    app.register_blueprint(main.bp)
+    
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
 
 
-
-
-
-import hmac
-
-def verify_signature(key, signature, body):
-    digest = hmac.new(key.encode("ascii"), msg=body, digestmod="sha256")
-    return hmac.compare_digest(f"sha256={digest.hexdigest().lower()}", signature.lower())
